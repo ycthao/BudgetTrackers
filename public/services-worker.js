060c4ad826fa4fb8185b68d5465467c4f5358e6d
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-02-24 23:15:56 -0600

    Finishing to define activate call

--- public/services-worker.js
+++ public/services-worker.js
@@ -19,7 +19,22 @@ self.addEventListener("install", function (evt) {
 });
 
 // activate
-self.addEventListener("activate", function (evt) {});
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
 
 // fetch
 self.addEventListener("fetch", function (evt) {});
