44bc1bbc8a0c78c0eb2f9290cb803296a20d3c9e
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-02-24 22:25:46 -0600

    Adding all files to services workers

--- public/services-worker.js
+++ public/services-worker.js
@@ -1,3 +1,11 @@
 const CACHE_NAME = "static-cache";
 const DATA_CACHE_NAME = "data-cache";
-const FILES_TO_CACHE = ["/", "/index.html"];
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
